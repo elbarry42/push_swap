53a52ee4da5d5dfbfb9f317867f7638dc8e70fd5
Luc Archimbaud <larchimb@z3r10p5.42lyon.fr>
2026-01-08 13:09:55 +0100

    changes on struc

--- include/push_swap.h
+++ include/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elbarry <elbarry@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: larchimb <larchimb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 17:26:03 by elbarry           #+#    #+#             */
-/*   Updated: 2026/01/06 16:24:28 by elbarry          ###   ########.fr       */
+/*   Updated: 2026/01/08 11:57:22 by larchimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,25 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-typedef struct s_node
-{
-	int				value;
-	//int				index;
-	struct s_node	*prev;
-	struct s_node	*next;
-} t_node;
+// typedef struct s_node
+// {
+// 	int				value;
+// 	//int				index;
+// 	struct s_node	*prev;
+// 	struct s_node	*next;
+// } t_node;
 
 typedef struct s_stack
 {
-	t_node	*top;
-	int		size;
+	int		value;
+	struct	s_stack	*prev;
+	struct	s_stack	*next;
 } t_stack;
 
 typedef struct s_push_swap
 {
-	t_stack stack_a;
-	t_stack	stack_b;
+	t_stack *stack_a;
+	t_stack	*stack_b;
 } t_ps;
 
 #endif
