27a998dbed71276c2751d4e4d0731e3a3fe6bdf6
Elhadj Amadou Barry <elbarry@z4r6p2.42lyon.fr>
2026-01-20 15:44:32 +0100

    bench

--- srcs/utils/bench.c
+++ srcs/utils/bench.c
@@ -6,55 +6,40 @@
 /*   By: elbarry <elbarry@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 16:11:23 by elbarry           #+#    #+#             */
-/*   Updated: 2026/01/19 18:28:00 by elbarry          ###   ########.fr       */
+/*   Updated: 2026/01/20 14:47:25 by elbarry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/libft.h"
 #include "../../include/push_swap.h"
 
-void	*ft_memset(void *s, int c, size_t n)
-{
-	size_t			i;
-	unsigned char	*s_bis;
-
-	i = 0;
-	s_bis = (unsigned char *) s;
-	while (i < n)
-	{
-		s_bis[i] = (unsigned char) c;
-		i++;
-	}
-	return (s);
-}
-
-static void	increment_stats(char *op, t_bench *b)
+static void	increment_stats(char *op, t_ps *ps)
 {
 	if (!ft_strcmp(op, "sa"))
-		b->sa++;
+		ps->bench->sa++;
 	else if (!ft_strcmp(op, "sb"))
-		b->sb++;
+		ps->bench->sb++;
 	else if (!ft_strcmp(op, "ss"))
-		b->ss++;
+		ps->bench->ss++;
 	else if (!ft_strcmp(op, "pa"))
-		b->pa++;
+		ps->bench->pa++;
 	else if (!ft_strcmp(op, "pb"))
-		b->pb++;
+		ps->bench->pb++;
 	else if (!ft_strcmp(op, "ra"))
-		b->ra++;
+		ps->bench->ra++;
 	else if (!ft_strcmp(op, "rb"))
-		b->rb++;
+		ps->bench->rb++;
 	else if (!ft_strcmp(op, "rr"))
-		b->rr++;
+		ps->bench->rr++;
 	else if (!ft_strcmp(op, "rra"))
-		b->rra++;
+		ps->bench->rra++;
 	else if (!ft_strcmp(op, "rrb"))
-		b->rrb++;
+		ps->bench->rrb++;
 	else if (!ft_strcmp(op, "rrr"))
-		b->rrr++;
+		ps->bench->rrr++;
 }
 
-void	bench_apply(char *op, t_ps *ps, t_bench *b)
+void	bench_apply(char *op, t_ps *ps)
 {
 	if (!ft_strcmp(op, "sa"))
 		sa(&ps->stack_a);
@@ -79,42 +64,20 @@ void	bench_apply(char *op, t_ps *ps, t_bench *b)
 	else if (!ft_strcmp(op, "rrr"))
 		rrr(&ps->stack_a, &ps->stack_b);
 
-	increment_stats(op, b);
-	b->total++;
+	increment_stats(op, ps);
+	ps->bench->total++;
 }
 
-void	print_bench(t_bench *b)
+void	print_bench(t_ps *ps)
 {
-	ft_putstr_fd("[bench] disorder:   ", 2);
-	ft_putdouble_fd(b->disorder * 100, 2);
-	ft_putstr_fd("%\n", 2);
-	ft_putstr_fd("[bench] strategy:   ", 2);
-	ft_putstr_fd(b->strategy_name, 2);
-	ft_putstr_fd(" / ", 2);
-	ft_putstr_fd(b->complexity_class, 2);
-	ft_putstr_fd("\n[bench] total_ops:  ", 2);
-	ft_putnbr_fd(b->total, 2);
-	ft_putstr_fd("\n[bench] sa: ", 2);
-	ft_putnbr_fd(b->sa, 2);
-	ft_putstr_fd(" sb: ", 2);
-	ft_putnbr_fd(b->sb, 2);
-	ft_putstr_fd(" ss: ", 2);
-	ft_putnbr_fd(b->ss, 2);
-	ft_putstr_fd(" pa: ", 2);
-	ft_putnbr_fd(b->pa, 2);
-	ft_putstr_fd(" pb: ", 2);
-	ft_putnbr_fd(b->pb, 2);
-	ft_putstr_fd("\n[bench] ra: ", 2);
-	ft_putnbr_fd(b->ra, 2);
-	ft_putstr_fd(" rb: ", 2);
-	ft_putnbr_fd(b->rb, 2);
-	ft_putstr_fd(" rr: ", 2);
-	ft_putnbr_fd(b->rr, 2);
-	ft_putstr_fd(" rra: ", 2);
-	ft_putnbr_fd(b->rra, 2);
-	ft_putstr_fd(" rrb: ", 2);
-	ft_putnbr_fd(b->rrb, 2);
-	ft_putstr_fd(" rrr: ", 2);
-	ft_putnbr_fd(b->rrr, 2);
-	ft_putchar_fd('\n', 2);
+	ft_printf("[bench] disorder : %d,%d\n", (int)ps->bench->disorder * 100,
+		(int)ps->bench->disorder * 10000 - ((int)ps->bench->disorder * 100) * 100);
+	ft_printf("[bench] total_ops : %d\n\n", ps->bench->total);
+
+	ft_printf("[bench] sa:%d sb:%d ss:%d pa:%d pb:%d\n",
+		ps->bench->sa, ps->bench->sb, ps->bench->ss, ps->bench->pa, ps->bench->pb);
+
+	ft_printf("[bench] ra:%d rb:%d rr:%d rra:%d rrb:%d rrr:%d\n",
+		ps->bench->ra, ps->bench->rb, ps->bench->rr, ps->bench->rra, ps->bench->rrb, ps->bench->rrr);
 }
+
--- srcs/utils/bench_utils.c
+++ srcs/utils/bench_utils.c
@@ -6,53 +6,12 @@
 /*   By: elbarry <elbarry@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 13:03:30 by elbarry           #+#    #+#             */
-/*   Updated: 2026/01/19 15:22:51 by elbarry          ###   ########.fr       */
+/*   Updated: 2026/01/20 15:42:16 by elbarry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/libft.h"
 #include "../../include/push_swap.h"
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write (fd, &c, 1);
-}
-
-void	ft_putstr_fd(char *s, int fd)
-{
-	int	i;
-
-	if (!s)
-	{
-		write(2, "null", 4);
-		return ;
-	}
-	i = 0;
-	while (s[i])
-	{
-		ft_putchar_fd(s[i], fd);
-		i++;
-	}
-}
-
-void	ft_putnbr_fd(int n, int fd)
-{
-	long	tmp;
-
-	tmp = n;
-	if (tmp < 0)
-	{
-		tmp = -tmp;
-		ft_putchar_fd('-', fd);
-	}
-	if (tmp < 10)
-		ft_putchar_fd((tmp + 48), fd);
-	else
-	{
-		ft_putnbr_fd((tmp / 10), fd);
-		ft_putchar_fd(((tmp % 10) + 48), fd);
-	}
-}
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
@@ -67,28 +26,29 @@ int	ft_strcmp(const char *s1, const char *s2)
 	return (s1[i] - s2[i]);
 }
 
-void	ft_putdouble_fd(double n, int fd)
+void	*ft_memset(void *s, int c, size_t n)
 {
-	int	integer_part;
-	int	decimal_part;
+	size_t			i;
+	unsigned char	*s_bis;
 
-	if (n < 0)
+	i = 0;
+	s_bis = (unsigned char *) s;
+	while (i < n)
 	{
-		ft_putchar_fd('-', fd);
-		n = -n;
+		s_bis[i] = (unsigned char) c;
+		i++;
 	}
-	integer_part = (int)n;
-	// On récupère les 2 chiffres après la virgule
-	decimal_part = (int)((n - integer_part) * 100 + 0.5);
-	ft_putnbr_fd(integer_part, fd);
-	ft_putchar_fd('.', fd);
-	if (decimal_part < 10)
-		ft_putchar_fd('0', fd);
-	ft_putnbr_fd(decimal_part, fd);
+	return (s);
 }
 
-// #include <stdio.h>
-// int main(void)
-// {
-// 	printf("%d", ft_strcmp(NULL, "barry"));
-// }
+char *detect_complexity(int ops, int n)
+{
+	double ratio;
+
+	ratio = (double)ops / n;
+	if (ratio < 5)
+		return ("O(n)");
+	if (ratio < 20)
+		return ("O(n log n)");
+	return ("O(n²)");
+}
